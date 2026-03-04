ad88b2836f6d891455ea5d3e4e137a03989b7925
MrMarco74 <webmaster@highantdev.de>
2026-03-04 23:12:30 +0100

    Release v1.18.7 (Beta): YADS 1.18.7: Bug Fix Release

--- yads/config.py
+++ yads/config.py
@@ -6,7 +6,7 @@ import re
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "YADS"
-    VERSION: str = "1.18.6"
+    VERSION: str = "1.18.7"
     DEBUG: bool = os.getenv("DEBUG", "false").lower() == "true"
     
     # Database
