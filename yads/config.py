0207a2a7a9f4ac7e4e55a7b7f4e152f3a542c208
MrMarco74 <webmaster@highantdev.de>
2026-03-07 09:36:18 +0100

    Release v1.19.1 (Beta): Stability and Timeout Enhancements

--- yads/config.py
+++ yads/config.py
@@ -6,7 +6,7 @@ import re
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "YADS"
-    VERSION: str = "1.19.0"
+    VERSION: str = "1.19.1"
     DEBUG: bool = os.getenv("DEBUG", "false").lower() == "true"
     
     # Database
