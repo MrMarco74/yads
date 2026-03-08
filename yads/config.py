907b1f69df52b49314f5b70c79cd31e48bbc4e5c
MrMarco74 <webmaster@highantdev.de>
2026-03-08 08:15:31 +0100

    Release v1.20.1 (Beta): Subdomain Scan Control

--- yads/config.py
+++ yads/config.py
@@ -6,7 +6,7 @@ import re
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "YADS"
-    VERSION: str = "1.20.0"
+    VERSION: str = "1.20.1"
     DEBUG: bool = os.getenv("DEBUG", "false").lower() == "true"
     
     # Database
