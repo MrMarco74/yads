8df72443d44a865ebf8240aa88af6ab967a4940e
MrMarco74 <webmaster@highantdev.de>
2026-03-08 14:21:17 +0100

    Release v1.21.3 (Beta): Stability and Reliability Enhancements

--- yads/config.py
+++ yads/config.py
@@ -6,7 +6,7 @@ import re
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "YADS"
-    VERSION: str = "1.21.2"
+    VERSION: str = "1.21.3"
     DEBUG: bool = os.getenv("DEBUG", "false").lower() == "true"
     
     # Database
