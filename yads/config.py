942a17724249e4e46e0444db7f6125cfbdcc605a
MrMarco74 <webmaster@highantdev.de>
2026-03-08 13:42:33 +0100

    Release v1.21.2 (Beta): Bug Fixes for Core Components

--- yads/config.py
+++ yads/config.py
@@ -6,7 +6,7 @@ import re
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "YADS"
-    VERSION: str = "1.21.0"
+    VERSION: str = "1.21.2"
     DEBUG: bool = os.getenv("DEBUG", "false").lower() == "true"
     
     # Database
