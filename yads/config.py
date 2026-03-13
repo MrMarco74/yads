73cfddf2f2beac0890a0e433408f1bc43e003d36
MrMarco74 <webmaster@highantdev.de>
2026-03-13 22:57:59 +0100

    Release v1.45.2: Local Setup Fix

--- yads/config.py
+++ yads/config.py
@@ -6,7 +6,7 @@ import re
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "YADS"
-    VERSION: str = "1.45.1"
+    VERSION: str = "1.45.2"
     DEBUG: bool = os.getenv("DEBUG", "false").lower() == "true"
     
     # Database
