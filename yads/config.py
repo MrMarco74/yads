98122cf125d325442ec5933567d4b02a3b5be12d
MrMarco74 <webmaster@highantdev.de>
2026-03-15 22:00:37 +0100

    Release v1.51.4: Enhancements and Registry Management

--- yads/config.py
+++ yads/config.py
@@ -7,7 +7,7 @@ import re
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "YADS"
-    VERSION: str = "1.51.3"
+    VERSION: str = "1.51.4"
     DEBUG: bool = os.getenv("DEBUG", "false").lower() == "true"
     
     # Database
