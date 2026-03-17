7d0b992632bae7729e209683aa1b00fba3a4bed4
MrMarco74 <webmaster@highantdev.de>
2026-03-18 00:15:43 +0100

    Release v2.0.6: Fresh Install Stability Fix

--- yads/config.py
+++ yads/config.py
@@ -7,7 +7,7 @@ import re
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "YADS"
-    VERSION: str = "2.0.5"
+    VERSION: str = "2.0.6"
     DEBUG: bool = os.getenv("DEBUG", "false").lower() == "true"
     
     # Database
