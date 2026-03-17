d833fe062cce29bb541c265139a08626f17936a8
MrMarco74 <webmaster@highantdev.de>
2026-03-17 23:26:01 +0100

    Release v2.0.4: Setup Wizard Stability

--- yads/config.py
+++ yads/config.py
@@ -7,7 +7,7 @@ import re
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "YADS"
-    VERSION: str = "2.0.3"
+    VERSION: str = "2.0.4"
     DEBUG: bool = os.getenv("DEBUG", "false").lower() == "true"
     
     # Database
