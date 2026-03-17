598cb9b3157caa672a4db01cf2a17d7eafab5131
MrMarco74 <webmaster@highantdev.de>
2026-03-17 22:44:14 +0100

    Release v2.0.1: Improved Setup Responsiveness

--- yads/config.py
+++ yads/config.py
@@ -7,7 +7,7 @@ import re
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "YADS"
-    VERSION: str = "2.0.0"
+    VERSION: str = "2.0.1"
     DEBUG: bool = os.getenv("DEBUG", "false").lower() == "true"
     
     # Database
