94e6e58c72e12ced1d832135f945e43e274b9376
MrMarco74 <webmaster@highantdev.de>
2026-03-18 12:20:13 +0100

    Release v2.1.0: Installer and Activation Fixes

--- yads/config.py
+++ yads/config.py
@@ -7,7 +7,7 @@ import re
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "YADS"
-    VERSION: str = "2.0.10"
+    VERSION: str = "2.1.0"
     DEBUG: bool = os.getenv("DEBUG", "false").lower() == "true"
     
     # Database
