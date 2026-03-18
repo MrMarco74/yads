0ccea56a18e6ddc72c9922fbdd83669ad30d56c3
MrMarco74 <webmaster@highantdev.de>
2026-03-18 07:30:40 +0100

    feat: release v2.0.8 with various bug fixes, improvements, and updated documentation.

--- yads/config.py
+++ yads/config.py
@@ -7,7 +7,7 @@ import re
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "YADS"
-    VERSION: str = "2.0.7"
+    VERSION: str = "2.0.8"
     DEBUG: bool = os.getenv("DEBUG", "false").lower() == "true"
     
     # Database
