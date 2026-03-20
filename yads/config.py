6cf5644a0170456d72c7ea83c472ca8f58e7ab4b
MrMarco74 <webmaster@highantdev.de>
2026-03-20 17:32:10 +0100

    Release v2.5.2: Release 2.5.2

--- yads/config.py
+++ yads/config.py
@@ -7,7 +7,7 @@ import re
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "YADS"
-    VERSION: str = "2.5.1"
+    VERSION: str = "2.5.2"
     DEBUG: bool = os.getenv("DEBUG", "false").lower() == "true"
     CORS_ALLOWED_ORIGINS: list[str] = os.getenv("CORS_ALLOWED_ORIGINS", "*").split(",")
 
