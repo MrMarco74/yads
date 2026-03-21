6481a27a0b7a3f028d3f5be28651859c82f582ef
MrMarco74 <webmaster@highantdev.de>
2026-03-21 23:46:03 +0100

    Release v2.5.6: MFA and Installer Improvements

--- yads/config.py
+++ yads/config.py
@@ -7,7 +7,7 @@ import re
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "YADS"
-    VERSION: str = "2.5.5"
+    VERSION: str = "2.5.6"
     DEBUG: bool = os.getenv("DEBUG", "false").lower() == "true"
     CORS_ALLOWED_ORIGINS: list[str] = os.getenv("CORS_ALLOWED_ORIGINS", "*").split(",")
 
