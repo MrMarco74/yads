792180eadb6d331dbd6363680c5ff3677561d1ec
MrMarco <marco@familie-frischkorn.de>
2026-08-11 11:23:36 +0200

    fix: sync VERSION string in config.py to 1.26.1

--- yads/config.py
+++ yads/config.py
@@ -7,7 +7,7 @@ import re
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "YADS"
-    VERSION: str = "2.6.5"
+    VERSION: str = "1.26.0"
     DEBUG: bool = os.getenv("DEBUG", "false").lower() == "true"
     CORS_ALLOWED_ORIGINS: list[str] = os.getenv("CORS_ALLOWED_ORIGINS", "*").split(",")
 
