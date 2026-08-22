435622a4382d08b7e05e5cc76a4537229ed675c2
MrMarco <marco@familie-frischkorn.de>
2026-08-22 21:59:31 +0200

    release: bump VERSION string in config.py to 1.28.1

--- yads/config.py
+++ yads/config.py
@@ -7,7 +7,7 @@ import re
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "YADS"
-    VERSION: str = "1.28.0"
+    VERSION: str = "1.28.1"
     DEBUG: bool = os.getenv("DEBUG", "false").lower() == "true"
     CORS_ALLOWED_ORIGINS: list[str] = os.getenv("CORS_ALLOWED_ORIGINS", "*").split(",")
 
